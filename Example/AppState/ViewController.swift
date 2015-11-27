43af4f45930377a469e47d1ede0366cff1b4f683
Viet Phan <viet.phanhong@asnet.com.vn>
2015-11-27 09:24:49 +0700

    Initial commit

---
+++ Example/AppState/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppState
+//
+//  Created by Viet Phan on 11/27/2015.
+//  Copyright (c) 2015 Viet Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
