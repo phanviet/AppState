188c935e5988cae08a24e0517cb84e237c4f32f6
Viet Phan <viet.phanhong@asnet.com.vn>
2016-01-06 16:32:27 +0700

    bump 0.2.0

--- Example/Pods/Target Support Files/Pods-AppState_Example/Pods-AppState_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-AppState_Example/Pods-AppState_Example-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods/AppState.framework"
+  install_framework "Pods-AppState_Example/AppState.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods/AppState.framework"
+  install_framework "Pods-AppState_Example/AppState.framework"
 fi
